26da81dbed2acd43cea14f5bb90d9cc5e1780404
Jia <233103498@qq.com>
2021-11-08 08:53:35 +0800

    增加ape 收藏图标

---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        text: '测开之路',
+        items: [
+            {text: '初级', link: '/dev/guide/junior/'},
+            {text: '中级', link: '/dev/guide/intermediate/'},
+            {text: '高级', link: '/dev/guide/senior/'}
+        ]
+    },
+    {
+        text: '开发语言',
+        items: [
+            {text: 'Java', link: '/dev/language/java/'},
+            {text: 'Python', link: '/dev/language/python/'},
+            {text: 'Golang', link: '/dev/language/go/'}
+        ]
+    },
+    {
+        text: '课程',
+        link: 'https://54tester.com'
+    },
+    {
+        text: 'Github',
+        link: 'https://github.com/emaolv/blog'
+    },
+]
