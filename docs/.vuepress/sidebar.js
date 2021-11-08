dd493a26f3935775983c02a82e2588a6777d04b4
Jia <233103498@qq.com>
2021-11-08 08:54:17 +0800

    优化侧边栏

---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,8 @@
+module.exports = {
+    '/dev/guide/junior/' : require('../dev/guide/junior/sidebar'),
+    '/dev/guide/intermediate/' : require('../dev/guide/intermediate/sidebar'),
+    '/dev/guide/senior/' : require('../dev/guide/senior/sidebar'),
+    '/dev/language/java/' : require('../dev/language/java/sidebar'),
+    '/dev/language/python/' : require('../dev/language/python/sidebar'),
+    '/dev/language/go/' : require('../dev/language/go/sidebar'),
+}
