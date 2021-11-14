5878ba398d3a218e0935e001c3c59dc35b3f9aaf
Jia <233103498@qq.com>
2021-11-14 17:20:31 +0800

    增加导航

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,5 +1,20 @@
 module.exports = [
     {
+        text: '电子书',
+        items: [
+            {text: '软件开发类', link: '/ebook/coo/'},
+            {text: '运营类', link: '/ebook/sdet/'}
+        ]
+    },
+    {
+        text: '读书笔记',
+        items: [
+            {text: '从零开始做内容：爆款内容的底层逻辑', link: '/ebook/coo/从零开始做内容/'},
+            {text: '抖音运营', link: '/ebook/coo/抖音运营/'},
+            {text: '秒赞', link: '/ebook/coo/秒赞/'}
+        ]
+    },
+    {
         text: '测开之路',
         items: [
             {text: '初级', link: '/dev/guide/junior/'},
