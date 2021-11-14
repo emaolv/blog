b8f634bbb7f4cde1aeaa13c37104c9c30abf963b
Jia <233103498@qq.com>
2021-11-14 17:21:01 +0800

    增加导航

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,4 +1,7 @@
 module.exports = {
+    '/ebook/coo/从零开始做内容/' : require('../ebook/coo/从零开始做内容/sidebar'),
+    '/ebook/coo/抖音运营/' : require('../ebook/coo/抖音运营/sidebar'),
+    '/ebook/coo/秒赞/' : require('../ebook/coo/秒赞/sidebar'),
     '/dev/guide/junior/' : require('../dev/guide/junior/sidebar'),
     '/dev/guide/intermediate/' : require('../dev/guide/intermediate/sidebar'),
     '/dev/guide/senior/' : require('../dev/guide/senior/sidebar'),
