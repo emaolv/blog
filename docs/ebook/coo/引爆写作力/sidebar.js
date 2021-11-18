d3f57997a3af96b2cce727e3ecdaa7c569ff7343
Jia <233103498@qq.com>
2021-11-18 22:00:18 +0800

    新增引爆写作力

---
+++ docs/ebook/coo/引爆写作力/sidebar.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        title: '引爆写作力',
+        collapsable: true,
+        children: [
+            '/ebook/coo/引爆写作力/第一章认知突围',
+            '/ebook/coo/引爆写作力/第二章文笔提升',
+            '/ebook/coo/引爆写作力/第三章铜牌写手写作指南',
+            '/ebook/coo/引爆写作力/第四章银牌写手写作指南',
+            '/ebook/coo/引爆写作力/第五章金牌写手写作指南',
+            '/ebook/coo/引爆写作力/第六章钻石写手写作指南',
+        ]
+    }
+]
