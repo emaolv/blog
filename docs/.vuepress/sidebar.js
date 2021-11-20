c0a3b558619e282d85977074dba652406cb390f4
Jia <233103498@qq.com>
2021-11-20 11:33:39 +0800

    增加玩转直播间

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -2,6 +2,7 @@ module.exports = {
     '/ebook/coo/从零开始做内容/' : require('../ebook/coo/从零开始做内容/sidebar'),
     '/ebook/coo/爆款抖音短视频/' : require('../ebook/coo/爆款抖音短视频/sidebar'),
     '/ebook/coo/引爆写作力/' : require('../ebook/coo/引爆写作力/sidebar'),
+    '/ebook/coo/玩转教育直播间/' : require('../ebook/coo/玩转教育直播间/sidebar'),
     '/ebook/sdet/饿了么质量体系搭建实战/' : require('../ebook/sdet/饿了么质量体系搭建实战/sidebar'),
     '/ebook/sdet/持续集成软件质量改进和风险降低之道/' : require('../ebook/sdet/持续集成软件质量改进和风险降低之道/sidebar'),
     '/dev/guide/junior/' : require('../dev/guide/junior/sidebar'),
