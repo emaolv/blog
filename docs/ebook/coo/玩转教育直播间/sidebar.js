ebbbd271615eb8b7364eb6944cddebffbb73579d
Jia <233103498@qq.com>
2021-11-20 11:34:47 +0800

    增加玩转教育直播间

---
+++ docs/ebook/coo/玩转教育直播间/sidebar.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        
+        title: '玩转教育直播间',
+        collapsable: true,
+        children: [
+            '/ebook/coo/玩转教育直播间/内容升级',
+            '/ebook/coo/玩转教育直播间/流量引入',
+            '/ebook/coo/玩转教育直播间/直播话术',
+        ]
+    }
+]
